48eac0cacf66b762f7a2486b1c7d9d44994b022a
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2020-03-13 13:16:55 +0530

    skeletal compoents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepotComponent } from './depot/depot.component';
+import { BusComponent } from './bus/bus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepotComponent,
+    BusComponent
   ],
   imports: [
     BrowserModule,
