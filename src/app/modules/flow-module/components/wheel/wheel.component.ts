e26d0edef18441ce349aae0dc6e89428ce4605d7
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2020-04-08 00:08:00 +0530

    cleanup

--- src/app/modules/flow-module/components/wheel/wheel.component.ts
+++ src/app/modules/flow-module/components/wheel/wheel.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Node } from '../../utils/TypeDefs';
-import { OverlayService } from '../../services/overlay.service';
 import { CONST_SELECTORS } from '../../utils/constants';
 
 @Component({
@@ -8,26 +7,6 @@ import { CONST_SELECTORS } from '../../utils/constants';
 	templateUrl: './wheel.component.html',
 	styleUrls: ['./wheel.component.scss']
 })
-export class WheelComponent implements OnInit {
-
+export class WheelComponent {
 	@Input() wheelData: Node.Wheel;
-	@Output('promote') newNodeAdderWheelEvtEmitter = new EventEmitter();
-	constructor(private overlayService: OverlayService) { }
-
-	ngOnInit(): void {
-	}
-
-	// TODO: Might have to be removed. No one is invoking this;
-	// onWheelClick(event: MouseEvent) {
-	// 	if (this.wheelData.descriptionPanel) {
-	// 		this.overlayService.open(<HTMLElement>event.target, this.wheelData.descriptionPanel, this.wheelData);
-	// 	} else {
-	// 		if (this.wheelData.promoter) {
-	// 			this.newNodeAdderWheelEvtEmitter.emit({
-	// 				wheelData: this.wheelData
-	// 			});
-	// 		}
-	// 	}
-	// }
-
 }
