0b690dca6cb4efc1ecafb8891a7ac0d7c5c44ec6
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2020-04-14 03:42:04 +0530

    new class for gneric operations

---
+++ projects/ng-flow-lib/src/utils/generic-ops.ts
@@ -0,0 +1,39 @@
+export const _ = {
+	attr(elm: HTMLElement, attr: string) {
+		return elm.attributes.getNamedItem(attr).value;
+	},
+
+	each(
+		o: Record<string, any>,
+		keySelector?: (k: any) => boolean | RegExp,
+		op?: "get" | "delete",
+		cb?: (k: string, v: any) => void
+	) {
+		const retO = {};
+		const cbs = [];
+		const keys = Object.keys(o);
+		if (cb) {
+			cbs.push(cb);
+		}
+		if (op === "delete") {
+			cbs.push(key => {
+				delete o[key];
+			});
+		} else if (op === "get") {
+		}
+		keys.forEach(key => {
+			let t = false;
+			if (
+				(typeof keySelector === "function" && keySelector(key)) ||
+				(keySelector && (<unknown>keySelector as RegExp).test(key)) ||
+				!keySelector
+			) {
+				cbs.forEach(cb => {
+					cb(key, o[key] || retO[key]);
+					retO[key] = o[key];
+				});
+			}
+		});
+		return retO;
+	}
+};
