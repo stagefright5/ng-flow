f158acf3a9e62f359ac3d6d64c86c5e4d5d7aa5f
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2020-04-14 03:52:05 +0530

    refactor(desc-panel): Wrapper for the overlayRef

--- projects/ng-flow-lib/src/services/overlay.service.ts
+++ projects/ng-flow-lib/src/services/overlay.service.ts
@@ -3,27 +3,34 @@ import { Overlay, OverlayRef, ConnectedPosition, OverlayConfig } from '@angular/
 import { ComponentPortal, ComponentType, PortalInjector } from '@angular/cdk/portal';
 import { Subscription } from 'rxjs';
 import { DESC_PANEL_DATA } from '../utils/constants';
+import { DescPanelRef } from '../utils/desc-panel-ref';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class OverlayService {
-	_overlayRef: OverlayRef;
+	_panelRef: DescPanelRef;
 	_backDropClickSub: Subscription;
-	_compRef: ComponentRef<unknown>;
+	_containerRef: ComponentRef<unknown>;
 
 	constructor(private _overlay: Overlay, private injector: Injector) { }
 
-	open(elToAttach: HTMLElement, comp: ComponentType<unknown>, config?: any) {
-		this._overlayRef = this._overlay.create(this._getOverlayConfig(elToAttach));
-		const customInjectors = this._createCustomInjector(config);
-		this._compRef = this._createAndAttachPanel(comp, customInjectors);
-		this.subscribeToBackDropClick();
+	open(elToAttach: HTMLElement, comp: ComponentType<unknown>, panelConfig?: any) {
+		const _overlayRef = this._createOverlay(elToAttach);
+		const _containerRef = this._attachPanelContainer(_overlayRef, comp, panelConfig);
+		const _panelRef = new DescPanelRef(_overlayRef, _containerRef, panelConfig);
+		return _panelRef;
 	}
 
-	_createAndAttachPanel(comp: ComponentType<unknown>, customInjectors?: PortalInjector) {
+	_createOverlay(elToAttach: HTMLElement) {
+		return this._overlay.create(this._getOverlayConfig(elToAttach));
+	}
+
+	_attachPanelContainer(_overlayRef: OverlayRef, comp: ComponentType<unknown>, config) {
+		const customInjectors = this._createCustomInjector(config);
 		const panelPortal = new ComponentPortal(comp, null, customInjectors);
-		return this._overlayRef.attach(panelPortal);
+		const containerRef = _overlayRef.attach(panelPortal);
+		return containerRef;
 	}
 
 	_createCustomInjector(config: any) {
@@ -37,8 +44,26 @@ export class OverlayService {
 			{
 				originX: 'center',
 				originY: 'bottom',
-				overlayX: 'center',
+				overlayX: 'end',
+				overlayY: 'top',
+			},
+			{
+				originX: 'start',
+				originY: 'top',
+				overlayX: 'start',
 				overlayY: 'bottom',
+			},
+			{
+				originX: 'start',
+				originY: 'top',
+				overlayX: 'end',
+				overlayY: 'bottom',
+			},
+			{
+				originX: 'start',
+				originY: 'bottom',
+				overlayX: 'start',
+				overlayY: 'top',
 			}
 		];
 		const positionStrategy = this._overlay.position()
@@ -48,36 +73,13 @@ export class OverlayService {
 			positionStrategy,
 			hasBackdrop: true,
 			disposeOnNavigation: true,
-			maxHeight: '5rem',
+			maxHeight: '30rem',
+			maxWidth: '20rem',
 			backdropClass: 'desc-panel-backdrop',
-			scrollStrategy: this._overlay.scrollStrategies.reposition({
-				autoClose: false
-			})
+			panelClass: 'desc-panel',
+			scrollStrategy: this._overlay.scrollStrategies.block()
 		}
 		return overlayConfig;
-	}
-
-	_detachPanel() {
-		this._overlayRef && this._overlayRef.detach();
-	}
-
-	_disposeOverlayRef() {
-		this._overlayRef && this._overlayRef.dispose();
-	}
-
-	private subscribeToBackDropClick() {
-		this._backDropClickSub = this._overlayRef.backdropClick().subscribe((...a) => {
-			this.close();
-		});
-	}
-
-	private close() {
-		if (this._compRef) {
-			this._compRef.destroy();
-		}
-		// this._detachPanel();
-		this._disposeOverlayRef();
-		this._backDropClickSub.unsubscribe();
 	}
 }
 
---
+++ projects/ng-flow-lib/src/utils/desc-panel-ref.ts
@@ -0,0 +1,44 @@
+import { OverlayRef } from '@angular/cdk/overlay';
+import { filter } from 'rxjs/operators';
+import { ESCAPE, hasModifierKey } from '@angular/cdk/keycodes'
+import { ComponentRef } from '@angular/core';
+
+export class DescPanelRef {
+	constructor(private _overlayRef: OverlayRef,
+		private _containerRef: ComponentRef<unknown>,
+		private _config: any) {
+		this._subscribeToKeyDownEvts();
+		this._subscribeToBackDropClick();
+	}
+
+	private _subscribeToBackDropClick() {
+		this._overlayRef.backdropClick().subscribe((...a) => {
+			this.close();
+		});
+	}
+
+	private _subscribeToKeyDownEvts() {
+		this._overlayRef.keydownEvents().pipe(
+			filter(event => {
+				return event.keyCode === ESCAPE && !this._config.disableClose && !hasModifierKey(event);
+			})
+		).subscribe(e => {
+			e.preventDefault();
+			this.close();
+		})
+	}
+
+	close() {
+		this._containerRef.destroy();
+		this._detachPanel();
+		this._disposeOverlayRef();
+	}
+
+	private _detachPanel() {
+		this._overlayRef && this._overlayRef.detach();
+	}
+
+	private _disposeOverlayRef() {
+		this._overlayRef && this._overlayRef.dispose();
+	}
+}
