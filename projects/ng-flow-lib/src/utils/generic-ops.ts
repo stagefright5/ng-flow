1dde0ea81f575342b2eebfb1818938344fae221c
Sankarshan A Joshi <sankarshan.joshi@neutrinos.co>
2020-04-17 01:31:12 +0530

    new functions for generating uuid

--- projects/ng-flow-lib/src/utils/generic-ops.ts
+++ projects/ng-flow-lib/src/utils/generic-ops.ts
@@ -35,5 +35,27 @@ export const _ = {
 			}
 		});
 		return retO;
+	},
+
+	generateUUID() {
+		function __s4() {
+			return Math.floor((1 + Math.random()) * 0x10000)
+				.toString(16)
+				.substring(1);
+		}
+		return (
+			__s4() +
+			__s4() +
+			"-" +
+			__s4() +
+			"-" +
+			__s4() +
+			"-" +
+			__s4() +
+			"-" +
+			__s4() +
+			__s4() +
+			__s4()
+		);
 	}
 };
